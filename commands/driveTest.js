d75efd7cb1ef6828d29610e7fdfb0ede0051cc6e
zsohn <50973129+zsohn@users.noreply.github.com>
2021-10-06 13:46:09 -0400

    outlined future dev in comments

--- commands/driveTest.js
+++ commands/driveTest.js
@@ -2,6 +2,18 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageAttachment, MessageEmbed } = require('discord.js');
 const fs = require('fs');
 
+
+//start with google drive path to images source folder
+//parse path
+//set active directory to root
+//recursively query the active directory for the next element of the path
+//once the id of the imgages source folder is located
+//list the images in image source folder
+//use math.rand to select a random element in the list
+//set fileid to that
+//profit
+
+
 var fileId = '16zLoK5c2JlivLxHp-15GcynsemHexm18';
 console.log("driveTest ln 6");
 var dest = fs.createWriteStream('./tmp/photo.png');
@@ -16,22 +28,14 @@ module.exports = {
     },
 };
 
-function attatchPhoto(interaction, drive){
-	try{
-        console.log("driveTest ln 21");
-        let resp = drive.files.get({
-            fileId: fileId,
-            alt: 'media'
-          });
-        resp.then( function (x) {
-            x;
-            console.log('Done');
-            })
-            .catch( function (err) {
-            console.log('Error during download', err);
-            })
-		interaction.reply({files: ["./tmp/photo.png"]});
-	} catch (error) {
-		console.error(error);
-	}
-}
+async function attatchPhoto(interaction, drive){
+    await drive.files.get(
+      { fileId, alt: 'media'},
+      { responseType: 'stream'}
+    ).then(res => {
+      res.data
+        .on('error', err => console.error('ERROR: Download failed.'))
+        .pipe(dest);
+    });
+    interaction.reply({files: ["./tmp/photo.png"]});
+  }
